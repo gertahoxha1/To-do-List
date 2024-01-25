b0d6f13bddbfe6a7b059c08ccb1bdaeed556f48c
gertahoxha1 <hoxha_gerta14@hotmail.com>
2024-01-25 17:09:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById ('inputField');
+
+addToDoButton.addEventListener('click',function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+    
+    
+})
+
+
+//shembulli i dyte
+
+// const fjalia = document.createElement("p");
+// fjalia.innerText = "Kjo eshte nje fjalia e dyte";
+
+// document.body.appendChild(fjalia);
+
+
+//shembulli i trete
+// function myFunction(){
+//     const list = document.getElementById("myDIV").classList;
+//     list.add("myStyle");
+//   }
